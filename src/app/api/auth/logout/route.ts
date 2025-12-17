1c1f00f6dc62dd8e525268277cf317041af9b0d8
donkene <nguimfackdilane@gmail.com>
2025-12-17 19:11:31 +0100

    ajout api logout

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,18 @@
+// Fichier: app/api/auth/logout/route.ts
+import { NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+
+export async function POST() {
+  try {
+    // suppression du cookie de session
+    cookies().delete('session_token')
+
+    return NextResponse.json({ 
+      success: true, 
+      message: "Déconnexion réussie" 
+    }, { status: 200 })
+
+  } catch (error) {
+    return NextResponse.json({ error: "Erreur serveur" }, { status: 500 })
+  }
+}
