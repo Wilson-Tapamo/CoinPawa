fc37a35e4c50396d9f3a2a65b12938e43e2efaef
Wilson-Tapamo <capucinesbetsi@gmail.com>
2026-01-23 15:20:32 +0100

    chore: add description to ts-expect-error for CI pass

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 // --- PATCH IMPORTANT ---
 // Permet de gérer les "BigInt" (Satoshis) sans faire planter l'API
-// @ts-expect-error
+// @ts-expect-error: Monkey patch BigInt to support JSON serialization
 BigInt.prototype.toJSON = function () { return this.toString() }
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
